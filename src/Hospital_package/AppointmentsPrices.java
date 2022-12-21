4939f6f76e75ec83227af6bbba5295f0b74dacc3
Akiradiazc <adiaz.laba@solvd.com>
2022-12-21 01:10:45 -0700

    Enum & Generics implementation (not ready)

---
+++ src/Hospital_package/AppointmentsPrices.java
@@ -0,0 +1,21 @@
+package Hospital_package;
+
+public enum AppointmentsPrices {
+
+    RADIOLOGY(400),
+    PEDIATRICS(200),
+    CARDIOLOGY(600),
+    DERMATOLOGY(600),
+    INTERNAL(800),
+    NEUROLOGY(800),
+    PNEUMOLOGY(600),
+    OPHTHALMOLOGY(500),
+    ONCOLOGY(500);
+
+    final int AppointmentPrice;
+
+    AppointmentsPrices(int AppointmentPrice){
+        this.AppointmentPrice = AppointmentPrice;
+    }
+
+}
